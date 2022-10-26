2e1ad87b004d58a813a5b1e5cd1d6ef1d45dcbe1
Uzair Saiyed <uzairhajra76330@gmail.com>
2022-10-26 19:13:46 +0530

    Heroku Fixes

--- public/js/imgError.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.nav__user-img')?.addEventListener('error', (e) => {
-	e.target.src = '../img/users/default-user.svg';
-});
-document.querySelector('.form__user-photo')?.addEventListener('error', (e) => {
-	e.target.src = '../img/users/default-user.svg';
-});
