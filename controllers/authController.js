a42bdc4695c73b1d1f730db38308884cebc7c504
Uzair Saiyed <uzairhajra76330@gmail.com>
2022-10-26 12:34:28 +0530

    Deployment Changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,17 +13,40 @@ const signToken = (id) => {
 		expiresIn: process.env.JWT_EXPIRES_IN
 	});
 };
-const createAndSendToken = (user, statusCode, res) => {
+// const createAndSendToken = (user, statusCode, res) => {
+// 	const token = signToken(user._id);
+// 	const cookieOptions = {
+// 		expires: new Date(
+// 			Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+// 		),
+// 		httpOnly: true,
+// 		secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
+// 	};
+// 	if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+// 	res.cookie('jwt', token, cookieOptions);
+
+// 	// Remove password from output
+// 	user.password = undefined;
+
+// 	res.status(statusCode).json({
+// 		status: 'success',
+// 		token,
+// 		data: {
+// 			user
+// 		}
+// 	});
+// };
+const createSendToken = (user, statusCode, req, res) => {
 	const token = signToken(user._id);
-	const cookieOptions = {
+
+	res.cookie('jwt', token, {
 		expires: new Date(
 			Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
 		),
-		httpOnly: true
-	};
-	if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
-
-	res.cookie('jwt', token, cookieOptions);
+		httpOnly: true,
+		secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
+	});
 
 	// Remove password from output
 	user.password = undefined;
@@ -36,7 +59,6 @@ const createAndSendToken = (user, statusCode, res) => {
 		}
 	});
 };
-
 exports.signup = catchAsync(async (req, res, next) => {
 	// const newUser = await User.create({
 	// 	name: req.body.name,
@@ -52,7 +74,7 @@ exports.signup = catchAsync(async (req, res, next) => {
 	}
 	// console.log(url);
 	await new Email(newUser, url).sendWelcome();
-	createAndSendToken(newUser, 201, res);
+	createAndSendToken(newUser, 201, req, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -66,7 +88,7 @@ exports.login = catchAsync(async (req, res, next) => {
 	if (!user || !(await user.correctPassword(password, user.password)))
 		return next(new AppError('Incorrect email or password'), 401);
 
-	createAndSendToken(user, 200, res);
+	createAndSendToken(user, 200, req, res);
 });
 
 exports.logout = catchAsync(async (req, res, next) => {
@@ -229,7 +251,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 	await user.save();
 	// 3. Update changedPasswordAt property for the user
 	// 4. Log the user in
-	createAndSendToken(user, 200, res);
+	createAndSendToken(user, 200, req, res);
 });
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
@@ -243,5 +265,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
 	user.passwordConfirm = req.body.passwordConfirm;
 	await user.save();
 	// 4. Log User in , send JWT
-	createAndSendToken(user, 200, res);
+	createAndSendToken(user, 200, req, res);
 });
