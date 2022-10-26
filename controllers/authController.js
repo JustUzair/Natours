3b63e7b188886fe6a859083b31751745e7aa60b8
Uzair Saiyed <uzairhajra76330@gmail.com>
2022-10-26 15:17:33 +0530

    Deployment Changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -38,13 +38,13 @@ const createAndSendToken = (user, statusCode, res) => {
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
-	const newUser = await User.create({
-		name: req.body.name,
-		email: req.body.email,
-		password: req.body.password,
-		passwordConfirm: req.body.passwordConfirm
-	});
-
+	// const newUser = await User.create({
+	// 	name: req.body.name,
+	// 	email: req.body.email,
+	// 	password: req.body.password,
+	// 	passwordConfirm: req.body.passwordConfirm
+	// });
+	const newUser = await User.create(req.body);
 	const url = `${req.protocol}://127.0.0.1:3000/me`;
 	if (process.env.NODE_ENV === 'production') {
 		url = `https://natours-uzair.herokuapp.com/me`;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,16 +4,16 @@ const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 
 router.post('/signup', authController.signup);
-
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);
 
-router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
+
+router.post('/forgotPassword', authController.forgotPassword);
 
 //Protect all the routes that are present after this middleware
 router.use(authController.protect);
-router.patch('/updateMyPassword', authController.updatePassword);
+
 router.get('/me', userController.getMe, userController.getUser);
 router.patch(
 	'/updateMe',
@@ -22,6 +22,8 @@ router.patch(
 	userController.updateMe
 );
 router.delete('/deleteMe', userController.deleteMe);
+
+router.patch('/updateMyPassword', authController.updatePassword);
 
 router.use(authController.restrictTo('admin'));
 router
